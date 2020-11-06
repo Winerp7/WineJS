bd2de0e18d84164d8514c4f081e0f8956318d1b6
fors0 <jensfors0@gmail.com>
2020-11-06 13:47:53 +0100

    delete all js files

--- dist/controllers/landingpageController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.landingpage = void 0;
-
-exports.landingpage = (_req, res) => {
-    res.render('landingpage', { title: 'Our dope ass app', path: '/' });
-};
