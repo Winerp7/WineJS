c7afb5a6efb1a3b4368db7068dccc210b8a85b72
Nicolai Steen Bjerring <nsbj17@student.aau.dk>
2020-11-06 14:54:39 +0100

    removing dist folder before merge

--- dist/controllers/landingpageController.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.landingpage = void 0;
-exports.landingpage = (req, res, next) => {
-    res.render('landingpage', { title: 'Our dope ass app', path: '/' });
-};
