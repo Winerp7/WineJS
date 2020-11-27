ab3c64553bcf61437295f0ae75497b9f74cc63d9
Jens Andersen <jensandersen@hey.com>
2020-11-27 09:41:25 +0100

    Delete test graph (#86)
    * Merge branch 'master' of https://github.com/fors0/WineJS into fontfun
    
    * removed test graph 💩💩💩💩
    
    Co-authored-by: fors0 <jensfors0@gmail.com>

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,6 @@
 import { Response, Request, NextFunction } from 'express';
 import { IUser, User } from "../models/userModel";
-import { makeCanvasLine, testGraph } from '../util/canni';
+import { makeCanvasLine} from '../util/canni';
 import { promisify } from 'es6-promisify';
 
 export const directFunctionality = async (req: Request, res: Response) => {
@@ -39,9 +39,8 @@ export const directDashboard = async (req: Request, res: Response) => {
     }
   }
 
-  const tGraph = await testGraph();
   let user = req.user as IUser;
-  res.render('dashboard', { pageTitle: 'Dashboard', path: '/dashboard', graphs: graphs, nodes: req.nodes, filter: user.filter, tGraph });
+  res.render('dashboard', { pageTitle: 'Dashboard', path: '/dashboard', graphs: graphs, nodes: req.nodes, filter: user.filter});
 };
 
 export const updateFilters = async (req: Request, res: Response) => {
--- src/util/canni.ts
+++ src/util/canni.ts
@@ -58,52 +58,6 @@ export const makeCanvasBar = async (title: string, xValues: Array<string>, yValu
   return dataUrl;
 };
 
-export const testGraph = async () => {
-  const configuration =  {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-  };
-
-  const dataUrl = await canvasRenderService.renderToDataURL(configuration);
-  const image = await canvasRenderService.renderToBuffer(configuration);
-  console.log(image);
-
-  return dataUrl;
-  
-}
-
-
-
 
 // https://www.chartjs.org/docs/latest/charts/
 export const makeCanvasLine = async (title: string, xValues: Array<string>, yValues: Array<number>) => {    
