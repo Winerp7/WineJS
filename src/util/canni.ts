e9995b51bc7bd5d0f60925c4988834fa9524a27f
Christopher Kirkegaard Sørensen <32838871+ChrisKS@users.noreply.github.com>
2020-11-30 10:55:37 +0100

    Now using chartist graphs (#91)
    * Merged master
    
    * Now using chartist

--- src/util/canni.ts
+++ src/util/canni.ts
@@ -1,114 +1,33 @@
-
-// chartjs-node-canvas example
-// https://github.com/SeanSobey/ChartjsNodeCanvas#Usage
-// https://github.com/SeanSobey/ChartjsNodeCanvas/blob/master/API.md
-// https://github.com/Automattic/node-canvas#canvastobuffer
-import { CanvasRenderService } from 'chartjs-node-canvas';
-
+const co = require('co');
+const generate = require('node-chartist');
 const width = 300;
 const height = 300;
-// TODO: give ChartJS the correct type
-const chartCallback = (ChartJS: any) => {
 
-    // Global config example: https://www.chartjs.org/docs/latest/configuration/
-    ChartJS.defaults.global.elements.rectangle.borderWidth = 2;
-    ChartJS.defaults.global.defaultFontFamily = "Sans"; // TODO Desperate attempt at making text appear
-    // Global plugin example: https://www.chartjs.org/docs/latest/developers/plugins.html
-    ChartJS.plugins.register({
-        // plugin implementation
-    });
-    // New chart type example: https://www.chartjs.org/docs/latest/developers/charts.html
-    ChartJS.controllers.MyType = ChartJS.DatasetController.extend({
-        // chart implementation
-    });
-};
-const canvasRenderService = new CanvasRenderService(width, height, chartCallback);
-
-
-export const makeCanvasBar = async (title: string, xValues: Array<string>, yValues: Array<number>) => {
-  const configuration = {
-      type: 'bar',
-      data: {
-          labels: xValues,
-          datasets: [{
-              label: title,
-              data: yValues,
-              backgroundColor: 'rgba(93, 188, 210, 0.5)',
-              borderColor: 'rgba(93, 188, 210, 1)',
-              borderWidth: 1
-          }]
-      },
-      options: {
-        legend: {
-          labels: {
-            boxWidth: 0
-          }
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
+// Chartist examples
+// https://github.com/panosoft/node-chartist
+// http://gionkunz.github.io/chartist-js/api-documentation.html
+// source of yoinked css: https://github.com/dsibilly/chartist-test/blob/master/main.css
+export const makeLine = co.wrap(function * (title: string, xValues: Array<string>, yValues: Array<number>) {
+  const options = {
+    width: width,
+    height: height,
+    axisX: {
+      labelInterpolationFnc: function(value: string, index: number) {
+        return index % (xValues.length > 4 ? (Math.ceil(xValues.length / 5)) : 1) === 0 ? value : null;
       }
-  };
-  //const image = await canvasRenderService.renderToBuffer(configuration);
-  const dataUrl = await canvasRenderService.renderToDataURL(configuration);
-  //const stream = canvasRenderService.renderToStream(configuration);
-  return dataUrl;
-};
-
-
-// https://www.chartjs.org/docs/latest/charts/
-export const makeCanvasLine = async (title: string, xValues: Array<string>, yValues: Array<number>) => {    
-  const configuration = {
-      type: 'line',
-      data: {
-          labels: xValues,
-          datasets: [{
-              label: title,
-              data: yValues,
-              fill: false,
-              pointBackgroundColor: 'rgba(93, 188, 210, 1)',
-              borderColor: [
-                  'rgba(93, 188, 210, 1)',
-              ]
-          }],
-      },
-      options: {
-        legend: {
-          labels: {
-            boxWidth: 0,
-          }
-        }
-      }
-  };
-  const dataUrl = await canvasRenderService.renderToDataURL(configuration);
-  return dataUrl;
-};
-
-export const makeCanvasDoughnut = async () => {
-  const configuration = {
-    type: 'doughnut',
-    data: {
-      datasets: [{
-        data: [10, 20, 30, 40],
-        backgroundColor: [
-          'rgba(255,0,0,1)',
-          'rgba(0,255,0,1)',
-          'rgba(0,0,255,1)',
-          'rgba(255,255,0,1)',
-        ],
-      }],
-      labels: [
-        '1st',
-        '2nd',
-        '3rd',
-        '4th',
-      ]
+    },
+    chartPadding: {
+      bottom: -10
     }
   };
-  const dataUrl = await canvasRenderService.renderToDataURL(configuration);
-  return dataUrl;
-};
+
+  const line = yield generate('line', options, {
+    labels: xValues,
+    series: [
+      {name: title, value: yValues},
+      //{name: 'Series 2', value: [3, 4, 5, 6, 7]}
+    ]
+  });
+
+  return line;
+})
