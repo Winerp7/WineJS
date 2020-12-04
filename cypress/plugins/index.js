1e9a0f5cbf270c90f9499a0b1803de8afdcb3055
fors0 <jensfors0@gmail.com>
2020-12-04 18:45:32 +0100

    added some test, and made login to an API call instead of visual

---
+++ cypress/plugins/index.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+// Importing the Required Modules
+const fs = require('fs');
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+  var envFileArr = fs.readFileSync(`${process.cwd()}/variables.env`, 'utf8').split('\n');
+
+  const envs = []
+  envFileArr.forEach((env) => {
+    if (env.startsWith('TEST')) {
+      // Split by '=' and only take the part on the right side of '='
+      envs.push(env.split('=')[1])
+    }
+  })
+
+  console.log('AAAAAL: ', envs);
+
+  config.env.TEST_NAME = envs[0];
+  config.env.TEST_EMAIL = envs[1];
+  config.env.TEST_PASS = envs[2];
+  return config
+}
